d8e20b97656afc5351a35c2bba2997c7c7dea3b1
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2024-01-31 20:09:16 +0200

    added data file examples, search functions

---
+++ data.js
@@ -0,0 +1,29 @@
+export let data = {
+    "inst0001": {
+        "title": "Instalare certificat criptare UNIPASS pentru Outlook",
+        "tags": ["instalare", "unipass", "outlook", "email", "certificat", "criptare", "encryption"],
+        "instruction": [
+            "deschide x",
+            "porneste y si apoi> 12323> 123213> click dreapta > install",
+            "download v",
+            "trust center",
+            "https://web.whatsapp.com/",
+            "trimis pe skype",
+            "control quality"
+        ],
+        "last update": "31/01/2024"
+    },
+    "inst0002": {
+        "title": "Trimitere colet | self AWB Fan Courier",
+        "tags": ["trimitere", "colet", "awb", "fan courier"],
+        "instruction": [
+            "deschide self AWB",
+            "porneste y si apoi> 12323> 123213> click dreapta > install",
+            "adauga agentia",
+            "completeaza un numar de telefon ex 01033023032",
+            "https://web.whatsapp.com/",
+            "trimis cu persoana de contact, nume si numar tel",
+        ],
+        "last update": "31/01/2024"
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+import { data } from "./data.js"
+
+
+
+const searchBtn = document.getElementById("search-btn")
+
+searchBtn.addEventListener("click", function() {
+    renderResult(document.getElementById("search-input").value.toLowerCase())
+})
+
 const searchInput = document.getElementById("search-input")
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        renderResult(document.getElementById("search-input").value.toLowerCase())
+    }
+})
 
+const mainConainer = document.getElementById("test")
 
+function renderResult(userInput) {
+    let result = ""
+    for (let elem of Object.values(data)) {
+        if (isInputInData(userInput, elem.tags)) {
+            result += 
+            `
+            <h4>${elem.title}</h4>
+            ${elem.instruction}
+            <br>
+            <hr>
+            <p>${elem["last update"]}</p>
+            `
+        }
+    }
 
+    mainConainer.innerHTML = result
+}
 
-
+function isInputInData(input, tags) {
+    for (let tag of tags) {
+        if (input === tag) {
+            return true
+        }
+    }
+}
 
