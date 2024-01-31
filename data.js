4d09f0d07199a5315c947698ed11af00f6eadb6e
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2024-01-31 21:32:12 +0200

    added random colors to results & one more temp data

--- data.js
+++ data.js
@@ -2,15 +2,17 @@ export let data = {
     "inst0001": {
         "title": "Instalare certificat criptare UNIPASS pentru Outlook",
         "tags": ["instalare", "unipass", "outlook", "email", "certificat", "criptare", "encryption"],
-        "instruction": [
-            "deschide x",
-            "porneste y si apoi> 12323> 123213> click dreapta > install",
-            "download v",
-            "trust center",
-            "https://web.whatsapp.com/",
-            "trimis pe skype",
-            "control quality"
-        ],
+        "instruction": 
+            `
+            acest text este temporar<br>
+            deschide x<br>
+            porneste y si apoi> 12323> 123213> click dreapta > install<br>
+            download v<br>
+            trust center<br>
+            <a href='https://web.whatsapp.com/'>https://web.whatsapp.com/</a><br>
+            trimis pe skype<br>
+            control quality<br>
+            `,
         "last update": "31/01/2024"
     },
     "inst0002": {
@@ -24,8 +26,24 @@ export let data = {
             completeaza un numar de telefon ex 01033023032<br>
             <a href='https://web.whatsapp.com/'>https://web.whatsapp.com/</a><br>
             trimis cu persoana de contact, nume si numar tel<br>
+            `,
+        "last update": "31/01/2024"
+    },
+    "inst0003": {
+        "title": "Adaugat in domeniu + Criptare laptop",
+        "tags": ["instalare", "unipass", "outlook", "email", "certificat", "criptare", "encryption"],
+        "instruction": 
             `
-        ,
+            acest text este temporar<br>
+            deschide laptop<br>
+            porneste y si apoi > laptop > cutie > incarcator HP > MBAM <br>
+            JoinDomain CLEAN<br>
+            adaugat in domeniu<br>
+            criptat cu MBAM<br>
+            API<br>
+            impachetat<br>
+            facut AWB<br>
+            `,
         "last update": "31/01/2024"
     }
 }
--- index.js
+++ index.js
@@ -29,10 +29,11 @@ function renderResult(userInput) {
     let result = ""
     for (let elem of Object.values(data)) {
         if (isInputInData(userInput, elem.tags)) {
+            const color = Math.floor(Math.random()*16777215).toString(16)
             result += 
             `
             <div class="d-flex text-body-secondary pt-3">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#${color}"></rect><text x="50%" y="50%" fill="#${color}" dy=".3em">32x32</text></svg>
             <p class="pb-3 mb-0 small lh-sm border-bottom">
               <strong class="d-block text-gray-dark">${elem.title}</strong>
               ${elem.instruction}
